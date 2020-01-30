ab47622c6b1c0564aa06c7391690ad1caca4ee52
elmira-balasanyan <elmira.balasanyan1995@gmail.com>
2020-01-31 00:02:16 +0400

    fixed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,10 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Sweet</title>
+    <title>Sweet
+
+
+    </title>
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
